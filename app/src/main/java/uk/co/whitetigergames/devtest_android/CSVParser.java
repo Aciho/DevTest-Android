be86fa94148f2e1032c3cdff1f94e1f080b284bc
Simon Watson <v2wm8ryazf@snkmail.com>
2015-08-09 15:33:13 +0100

    Unit testing set up and working.

---
+++ app/src/main/java/uk/co/whitetigergames/devtest_android/CSVParser.java
@@ -0,0 +1,12 @@
+package uk.co.whitetigergames.devtest_android;
+
+/**
+ * Created by Simon on 09/08/2015.
+ */
+public class CSVParser
+{
+    public boolean IsTrue()
+    {
+        return true;
+    }
+}
---
+++ app/src/test/java/CSVParserTest.java
@@ -0,0 +1,18 @@
+package uk.co.whitetigergames.devtest_android;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+/**
+ * Created by Simon on 09/08/2015.
+ */
+public class CSVParserTest
+{
+    @Test
+    public void DoesSomething()
+    {
+        CSVParser parser = new CSVParser();
+        assertEquals(true, parser.IsTrue());
+    }
+}
