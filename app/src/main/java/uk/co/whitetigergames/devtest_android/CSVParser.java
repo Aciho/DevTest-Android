16521cbc22ad221ba2e6a8f13697d827a7128e17
Simon Watson <v2wm8ryazf@snkmail.com>
2015-08-11 23:31:56 +0100

    Learned a lot about JUnit, and Java file IO First proper test written and passing.

--- app/src/main/java/uk/co/whitetigergames/devtest_android/CSVParser.java
+++ app/src/main/java/uk/co/whitetigergames/devtest_android/CSVParser.java
@@ -1,12 +1,41 @@
 package uk.co.whitetigergames.devtest_android;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+
 /**
  * Created by Simon on 09/08/2015.
  */
 public class CSVParser
 {
-    public boolean IsTrue()
-    {
-        return true;
+    public int Count;
+
+    public CSVParser(Reader input) throws IOException {
+        Count = 0;
+        BufferedReader bufferedReader = null;
+
+        try
+        {
+            String sCurrentLine;
+            bufferedReader = new BufferedReader(input);
+
+            bufferedReader.readLine(); //Discard title line
+            
+            while ((sCurrentLine = bufferedReader.readLine()) != null)
+            {
+                Count++;
+            }
+        }
+        catch (Exception ex)
+        {
+            System.out.println(ex.getMessage());
+        }
+        finally
+        {
+            bufferedReader.close();
+        }
     }
+
 }
